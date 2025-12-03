d03216fb9efa62d7db50bc9196228f794e007275
mhLan04 <lanmhgcs220608@fpt.edu.vn>
2025-12-03 14:50:34 +0700

    updatesourcecode

---
+++ vocab-builder/server/server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+global.Vocab = require('./api/models/vocabModel');
+const routes = require('./api/routes/vocabRoutes');
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb+srv://userid:userid@fgwweb2.uqebdxp.mongodb.net/?appName=FGWWeb2', 
+  { useNewUrlParser: true, useUnifiedTopology: true }
+);
+const port = process.env.PORT || 3000;
+const app = express();
+app.use(cors());
+app.use((req, res, next) => {
+  res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
+  res.setHeader('Pragma', 'no-cache');
+  res.setHeader('Expires', '0');
+  next();
+});
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+routes(app);
+app.listen(port);
+app.use((req, res) => {
+  res.status(404).send(`url: ${req.originalUrl} not found`);
+});
+
+console.log(`Server stared on port ${port}`);
